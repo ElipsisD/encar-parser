5fa9fd583ebe0cf97a938232e8f7dda532622800
boykoda <boykoda@cifra-k.ru>
2025-05-29 22:52:58 +0700

    feat: add work with selenium driver

--- app/app.py
+++ app/app.py
@@ -1,6 +1,15 @@
+import subprocess
 import time
+from functools import lru_cache
+
 import requests
 import os
+
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+from selenium import webdriver
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
@@ -12,9 +21,7 @@ def load_links(file_path):
 
 
 def fetch_data(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
-    }
+    headers = {"User-Agent": get_ua()}
     response = requests.get(url, headers=headers)
     if not response.ok:
         print(response.status_code)
@@ -79,7 +86,43 @@ def save_seen_ids(file_path, seen_ids):
             f.write(f"{item_id}\n")
 
 
+def selenium_start():
+    get_chrome_version()
+
+    def _initialize_webdriver() -> WebDriver:
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920x1080")
+        options.add_argument(f"user-agent={get_ua()}")
+        service = Service("/usr/bin/chromedriver")
+        return webdriver.Chrome(service=service, options=options)
+
+    driver = _initialize_webdriver()
+    driver.get(
+        "http://www.encar.com/dc/dc_carsearchlist.do?carType=kor#!%7B%22action%22%3A%22(And.Hidden.N._.Year.range(..202206)._.Mileage.range(..30000)._.(C.CarType.Y._.(C.Manufacturer.%ED%98%84%EB%8C%80._.(C.ModelGroup.%ED%88%AC%EC%8B%BC._.(C.Model.%ED%88%AC%EC%8B%BC%20(NX4_)._.BadgeGroup.%EA%B0%80%EC%86%94%EB%A6%B0%201600cc.)))))%22%2C%22toggle%22%3A%7B%7D%2C%22layer%22%3A%22%22%2C%22sort%22%3A%22ModifiedDate%22%2C%22page%22%3A1%2C%22limit%22%3A%2250%22%2C%22searchKey%22%3A%22%22%2C%22loginCheck%22%3Afalse%7D"
+    )
+    print("Selenium wait!")
+    driver.implicitly_wait(10)
+    print("Selenium ready!")
+
+
+@lru_cache
+def get_chrome_version() -> str:
+    return subprocess.run(
+        "google-chrome --product-version", shell=True, capture_output=True, text=True
+    ).stdout.strip()
+
+
+def get_ua() -> str:
+    chrome_version = get_chrome_version()
+    return f"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{chrome_version} Safari/537.36"
+
+
 def main():
+    selenium_start()
     links = load_links("links.txt")
     seen_ids = load_seen_ids("seen_ids.txt")
 
