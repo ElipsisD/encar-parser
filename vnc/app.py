b5d8fd4e797876dcdc9c88e42caa8f0b703b09cf
boykoda <boykoda@cifra-k.ru>
2025-06-08 15:41:24 +0700

    feat: add user agent setting

--- vnc/app.py
+++ vnc/app.py
@@ -1,12 +1,23 @@
+import subprocess
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+
+def get_chrome_version() -> str:
+    return subprocess.run(
+        "google-chrome --product-version", shell=True, capture_output=True, text=True
+    ).stdout.strip()
 
 options = Options()
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--start-maximized")
+options.add_argument(f"--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{get_chrome_version()} Safari/537.36")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+options.add_argument("--disable-blink-features=AutomationControlled")
 # options.add_argument("--headless=new")
 
 driver = webdriver.Chrome(options=options)
